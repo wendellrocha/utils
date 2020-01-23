d4703cbaf08df3f1762a2f90b67118afd6bf42d9
Wendell Rocha <wendellrocha@outlook.com>
2020-01-23 20:22:24 -0300

    Update historico.js

--- publish/historico.js
+++ publish/historico.js
@@ -2,44 +2,51 @@ Meteor.publish('Historico', function (id) {
     if (this.userId && id) {
         let doc = Mongo.Collection.get('pessoa').aggregate([
             {
-                $match: {
-                    id_usuario: id
-                }
-            },
-            {
-                $lookup: {
-                    from: 'pessoa.vermongo',
-                    localField: 'id_usuario',
-                    foreignField: 'id_usuario',
-                    as: 'versoes'
-                }
-            },
-            {
-                $unwind: '$versoes'
-            },
-            {
-                $project: {
-                    alteracoes: {
-                        email: '$versoes.email',
-                        nome: '$versoes.nome',
-                        sobrenome: '$versoes.sobrenome',
-                        ddd_cel: '$versoes.ddd_cel',
-                        celular: '$versoes.celular',
-                        avatar: '$versoes.avatar',
-                        data_nascimento: { $dateToString: { format: "%d/%m/%Y", date: "$versoes.data_nascimento" } },
-                        genero: '$versoes.genero',
-                        ativo: '$versoes.ativo',
-                        createdAt: { $dateToString: { format: "%d/%m/%Y", date: "$versoes.createdAt" } },
-                        modifiedAt: { $dateToString: { format: "%d/%m/%Y", date: "$versoes.modifiedAt" } }
-                    }
-                }
-            },
-            {
-                $group: {
-                    _id: '$alteracoes.modifiedAt',
-                    alteracoes: { $push: '$alteracoes' }
-                }
+        $match: {
+            id_usuario: id
+        }
+    },
+    {
+        $lookup: {
+            from: 'pessoa.vermongo',
+            localField: 'id_usuario',
+            foreignField: 'id_usuario',
+            as: 'versoes'
+        }
+    },
+    {
+        $unwind: '$versoes'
+    },
+    {
+        $project: {
+            ordem: { $dateToString: { format: "%Y%m%d", date: "$versoes.modifiedAt" } },
+            alteracoes: {
+                email: '$versoes.email',
+                nome: '$versoes.nome',
+                sobrenome: '$versoes.sobrenome',
+                ddd_cel: '$versoes.ddd_cel',
+                celular: '$versoes.celular',
+                avatar: '$versoes.avatar',
+                data_nascimento: { $dateToString: { format: "%d/%m/%Y", date: "$versoes.data_nascimento" } },
+                genero: '$versoes.genero',
+                ativo: '$versoes.ativo',
+                createdAt: { $dateToString: { format: "%d/%m/%Y", date: "$versoes.createdAt" } },
+                modifiedAt: { $dateToString: { format: "%d/%m/%Y", date: "$versoes.modifiedAt" } },
             }
+        }
+    },
+    {
+        $group: {
+            _id: '$alteracoes.modifiedAt',
+            ordem: { $last: '$ordem' },
+            alteracoes: { $push: '$alteracoes' }
+        }
+    },
+    {
+        $sort: {
+            'ordem': -1
+        }
+    }
         ]);
         let subscription = this;
 
