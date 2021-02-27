c317f4c19266194fb8d8a98c32873c349e9b9c95
Wendell Rocha <wendellrocha@outlook.com>
2021-02-27 13:00:15 -0300

    Update install-rpm-like.sh

--- linux/install-rpm-like.sh
+++ linux/install-rpm-like.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-sudo dnf get-updates
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf check-update
 sudo dnf update -y
 
-sudo dnf module install nodejs:12.20.1
 sudo dnf install java-1.8.0-openjdk.x86_64
 
 sudo touch /etc/yum.repos.d/mongodb-org.4.4.repo
 
-sudo sh -c 'echo -e "[mongodb-org-4.4]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.4/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc" > /etc/yum.repos.d/mongodb-org.4.4.repo'
+sudo sh -c 'echo -e "[mongodb-org-4.4]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/8Server/mongodb-org/4.4/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc" > /etc/yum.repos.d/mongodb-org.4.4.repo'
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
@@ -15,7 +20,11 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 sudo update-alternatives --config java
 
 sudo dnf check-update
-sudo dnf install -y mongodb-org-shell mongodb-org-mongos mongodb-org-tools code fira-code-fonts elixir
+sudo dnf install -y mongodb-org-shell mongodb-org-mongos mongodb-org-tools code fira-code-fonts elixir git zsh nodejs
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 mkdir -p Android/sdk
 mkdir -p .android && touch .android/repositories.cfg
@@ -49,6 +58,7 @@ curl https://install.meteor.com/ | sh
 wget -O dart-sdk.zip https://storage.googleapis.com/dart-archive/channels/stable/release/2.10.5/sdk/dartsdk-linux-x64-release.zip
 unzip dart-sdk.zip && rm dart-sdk.zip
 export PATH=$PATH:/home/wendellr/dart-sdk/bin
+export PATH=$PATH:/home/wendellr/.pub-cache/bin
 
 pub global activate fvm
 pub global activate slidy
